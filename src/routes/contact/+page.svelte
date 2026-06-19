94aa52bba7e667f3118913754338a40a7b8ef5ae
diamant <rurudiamant@gmail.com>
2026-06-19 13:56:02 +0200

    added services and contact pages

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,1 +1,21 @@
-<main class="mt-24 p-4">contact</main>
+<script>
+	import Button from '$lib/components/ui/button/button.svelte';
+</script>
+
+<main class="mt-24 p-4">
+	<div class="m-auto max-w-4xl">
+		<h2
+			class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+		>
+			Reach me
+		</h2>
+		<ul class="my-6 ms-6 list-disc [&>li]:mt-2">
+			<li>On whatsapp or simple call : +257 62 277 058</li>
+			<li>Email: starcode257@gmail.com || rurudiamant@gmail.com</li>
+			<li>Address : Muyaga, Bujumbura, Burundi, 1000</li>
+		</ul>
+		<a href="mailto:rurudiamant@gmail.com?subject=Hi&body=Hi, diamant"
+			><Button>Send me an email</Button></a
+		>
+	</div>
+</main>
--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -1,1 +1,53 @@
-<main class="mt-24 p-4">services</main>
+<main class="mt-24 p-4">
+	<section class="m-auto max-w-4xl">
+		<div class="mb-6">
+			<h2
+				class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+			>
+				Business Application
+			</h2>
+			<p class="leading-7 not-first:mt-6">I will develop a system for managing a business.</p>
+		</div>
+		<div class="mb-6">
+			<h2
+				class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+			>
+				Video Conferencing Platform
+			</h2>
+			<p class="leading-7 not-first:mt-6">
+				I will build a real-time communication video chat application.
+			</p>
+		</div>
+		<div class="mb-6">
+			<h2
+				class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+			>
+				Video Streaming Platform
+			</h2>
+			<p class="leading-7 not-first:mt-6">
+				I will build a web platform for uploading, managing, and streaming video content online.
+			</p>
+		</div>
+		<div class="mb-6">
+			<h2
+				class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+			>
+				Timetable Generator & Path Finder
+			</h2>
+			<p class="leading-7 not-first:mt-6">
+				I will develop a system that Automatically generates schedules or a best/good solution path
+				for a given problem.
+			</p>
+		</div>
+		<div class="mb-6">
+			<h2
+				class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+			>
+				Data Information Gathering System
+			</h2>
+			<p class="leading-7 not-first:mt-6">
+				Develop a system that can extract some information from given data (probably with AI).
+			</p>
+		</div>
+	</section>
+</main>
